b259e3d204a05926c89e6c87aaa3a7a7bdf39509
Aaron Chen <aaronchen@m800.com>
2024-08-01 13:59:51 +0800

    feat(chatRoom): use ref to controll scroll position

--- packages/app/components/ChatRoom.tsx
+++ packages/app/components/ChatRoom.tsx
@@ -1,3 +1,5 @@
+import { useEffect, useRef } from "react";
+
 import Message from "./Message";
 import ChatInput from "./ChatInput";
 
@@ -9,18 +11,40 @@ type ChatRoomProps = {
 };
 
 const ChatRoom = ({ currentUser }: ChatRoomProps) => {
+  const historyRef = useRef<HTMLDivElement>(null);
+  const shouldAutoScrolled = useRef<boolean>(true);
   const { sendTextMessage, messages } = useMessageAction({
     currentUser,
   });
 
   const handleTextMessage = (message: string) => {
     sendTextMessage(message);
-    // TODO: scroll to bottom
+    shouldAutoScrolled.current = true;
   };
+
+  useEffect(() => {
+    if (historyRef?.current) {
+      const scrollHeight: number = historyRef.current.scrollHeight;
+
+      if (shouldAutoScrolled.current) {
+        historyRef.current.scrollTop = scrollHeight;
+      }
+    }
+  }, [messages]);
+
+  useEffect(() => {
+    if (historyRef.current) {
+      historyRef.current.onscroll = (e: Event) => {
+        const scrollEl = e.currentTarget as HTMLDivElement;
+        shouldAutoScrolled.current =
+          scrollEl.scrollTop + scrollEl.clientHeight >= scrollEl.scrollHeight;
+      };
+    }
+  }, []);
 
   return (
     <div className="bg-white dark:bg-black w-screen flex flex-1 flex-col justify-between overflow-hidden">
-      <div className="flex-initial overflow-y-auto p-4">
+      <div ref={historyRef} className="flex-initial overflow-y-auto p-4">
         {messages.map((message: IMessage) => (
           <Message
             key={message.timestamp}
