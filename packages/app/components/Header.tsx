35822e15744cae5dcfc55ab14b06dbfd19e8fe48
regchiu <regx1385@gmail.com>
2024-08-09 17:20:01 +0800

    feat: adjust title font

--- packages/app/components/Header.tsx
+++ packages/app/components/Header.tsx
@@ -5,7 +5,7 @@ export default function Header() {
     <header className="border-b border-gray-200 dark:border-gray-600 bg-white dark:bg-gray-800 dark:text-white">
       <div className="container mx-auto px-2 sm:px-6 lg:px-8">
         <div className="relative flex h-16 items-center justify-between">
-          <div>Exam Local Chat</div>
+          <div className="text-2xl font-medium">Exam Local Chat</div>
           <DarkModeSwitcher />
         </div>
       </div>
