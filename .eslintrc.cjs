2da5b6f419f1fed7bca3a0979193880360dfe827
regchiu <regx1385@gmail.com>
2024-08-09 10:16:37 +0800

    chore: setup eslint-plugin-react
    add react/self-closing-comp to rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,14 +3,23 @@ module.exports = {
   env: { browser: true, es2020: true },
   extends: [
     "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
     "plugin:@typescript-eslint/strict",
     "plugin:react-hooks/recommended",
     "plugin:prettier/recommended",
   ],
   ignorePatterns: ["dist", ".eslintrc.cjs"],
   parser: "@typescript-eslint/parser",
-  plugins: ["eslint-plugin-tsdoc", "react-refresh"],
+  plugins: ["react", "eslint-plugin-tsdoc", "react-refresh"],
   rules: {
+    "react/self-closing-comp": [
+      "error",
+      {
+        component: true,
+        html: true,
+      },
+    ],
     "react-refresh/only-export-components": [
       "warn",
       { allowConstantExport: true },
