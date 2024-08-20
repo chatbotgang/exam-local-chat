b89464997ca4f078076ba9fcb7e344ec62fe7e03
shueny <shueny_wang@trendmicro.com>
2024-08-20 22:02:15 +0800

    chore: initial project setup

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,6 +1,6 @@
 const config = {
   "**/*": ["prettier --ignore-unknown --write"],
-  "**/*.{ts,tsx,js,jsx,cjs}": "eslint --fix",
+  "packages/app/**/*.{ts,tsx,js,jsx,cjs}": "eslint --fix",
   "packages/app/**/*.{ts,tsx}": () => "pnpm exec tsc -p packages/app",
 };
 
