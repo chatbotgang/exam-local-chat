46e2c6683dd33b3a922232e3e4c9d53cdc17b874
tw0517tw <tw0517tw@gmail.com>
2024-08-23 16:46:58 +0800

    chore: more readable hash function

--- packages/app/utils/index.tsx
+++ packages/app/utils/index.tsx
@@ -1,7 +1,7 @@
 export function generateRandomColor(username: string) {
   let hash = 0;
   for (let i = 0; i < username.length; i++) {
-    hash = username.charCodeAt(i) + ((hash << 5) - hash);
+    hash = username.charCodeAt(i) + hash * 31;
   }
 
   const hue = hash % 360;
