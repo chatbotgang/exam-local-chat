f2c4c53dcdddbc78d218cf85d4d6005a16aa4645
Liang C <sl20130313@gmail.com>
2024-09-23 00:01:09 +0800

    feat: enhance Message UI with MUI styling

--- packages/app/components/ChatRoom/Message.tsx
+++ packages/app/components/ChatRoom/Message.tsx
@@ -1,15 +1,52 @@
+import { Box, Typography } from "@mui/material";
 import type { FC } from "react";
 import { type ChatMessage, ChatMessageType } from "../../types/message";
 
 interface MessageProps {
+  localUsername: string;
   chatMessage: ChatMessage;
 }
 
-const Message: FC<MessageProps> = ({ chatMessage }) => {
-  return chatMessage.type === ChatMessageType.Text ? (
-    <div>{`${chatMessage.username}: ${chatMessage.message}`}</div>
+const Message: FC<MessageProps> = ({ localUsername, chatMessage }) => {
+  const { type, username } = chatMessage;
+  const isTextTypeMessage = type === ChatMessageType.Text;
+  const isLocalUserMessage = localUsername === username;
+
+  return isTextTypeMessage ? (
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: isLocalUserMessage ? "flex-end" : "flex-start",
+      }}
+    >
+      {!isLocalUserMessage && (
+        <Typography variant="h6">{username}: </Typography>
+      )}
+      <Box
+        sx={{
+          backgroundColor: isLocalUserMessage ? "primary.dark" : "grey.600",
+          color: "white",
+          padding: "8px 12px",
+          borderRadius: "8px",
+          margin: "8px 0",
+          maxWidth: "85%",
+        }}
+      >
+        <Typography
+          variant="body1"
+          sx={{ whiteSpace: "pre-wrap", wordBreak: "break-word" }}
+        >
+          {chatMessage.message}
+        </Typography>
+      </Box>
+    </Box>
   ) : (
-    <div>{`${chatMessage.type}: ${chatMessage.username}`}</div>
+    <Box sx={{ margin: "4px 0" }}>
+      <Typography variant="body1" align="center" sx={{ fontStyle: "italic" }}>
+        {`${chatMessage.username} ${chatMessage.type}`}
+      </Typography>
+    </Box>
   );
 };
 
